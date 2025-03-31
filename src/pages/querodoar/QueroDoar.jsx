bd5794eca69c68f61c9d6832927854c804c0a881
leticiasimoess <leticiasimoes16@gmail.com>
2025-03-31 16:37:01 -0300

    feat(querodoar): add new feature and fix layout issue

--- src/pages/querodoar/QueroDoar.jsx
+++ src/pages/querodoar/QueroDoar.jsx
@@ -11,9 +11,9 @@ export default function QueroDoar(){
     const [titulo,setTitulo] = useState("")
     const [categoria,setCategoria] = useState("")
     const [autor,setAutor] = useState("")
-    const [image_Url,setImage_Url] = useState("")
+    const [imagem_url,setImagem_url] = useState("")
 
-
+// o (e).target.value significa "pegue o que foi digitado"
     const capturarTitulo = (e) => {
         setTitulo(e.target.value)
     }
@@ -26,8 +26,8 @@ export default function QueroDoar(){
         setAutor(e.target.value)
     }
 
-    const capturarImagem = (e) => {
-       setImage_Url(e.target.value)
+    const capturarImagem_url = (e) => {
+        setImagem_url(e.target.value)
     }
 
     const enviarDados = async()=> {
@@ -36,24 +36,25 @@ export default function QueroDoar(){
             titulo,
             categoria,
             autor,
-            image_Url
+            imagem_url,
            }
 
            await axios.post("https://api-vai-no-livro.onrender.com/doar", dadosAEnviar)
     }
 
+
     return (
         <section className={s.queroDoarSection}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro.</p>
-            <form action="">
+            <form onSubmit={(e) => e.preventDefault()}>
                 <div>
                     <img src={iconeLivro} alt="Imagem com icone de livro aberto com borda azul" />
                     <h2>Informações do Livro</h2>
                 </div>
-                <input type="text" placeholder='Título' onChange={capturarTitulo} />
-                <input type="text" placeholder='Categoria' onChange={capturarCategoria}/>
-                <input type="text" placeholder='Autor'onChange={capturarAutor}/>
-                <input type="text" placeholder='Link da Imagem' onChange={capturarImagem}/>
+                <input type="text" placeholder='Título' onChange={capturarTitulo} required />
+                <input type="text" placeholder='Categoria' onChange={capturarCategoria} required />
+                <input type="text" placeholder='Autor'onChange={capturarAutor} required />
+                <input type="text" placeholder='Link da Imagem' onChange={capturarImagem_url} required />
                 <button type="submit" className={s.buttonDoar} onClick={enviarDados}>Doar</button>
             </form>
            
